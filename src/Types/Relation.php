d71a8905d44017199b7e76a9f8a29367f4634220
Patrick Morpey <patrick@morphp.nl>
2022-01-06 11:07:05 +0100

    Changed attribute parameter name

--- src/Types/Relation.php
+++ src/Types/Relation.php
@@ -8,7 +8,7 @@ namespace Medas\EntityManager\Types;
 class Relation extends BaseType
 {
     public function __construct(
-        public string $className
+        public string $entity
     )
     {
     }
