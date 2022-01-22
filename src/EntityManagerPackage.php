6552d79f0ad3c767d36fd25bc7f48a42aeb41caa
Patrick Morpey <patrick@morphp.nl>
2022-01-22 14:54:16 +0100

    Updated packages

--- src/EntityManagerPackage.php
+++ src/EntityManagerPackage.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Medas\EntityManager;
 
 use Medas\ConfigManager\ConfigManagerPackage;
+use Medas\ServiceManager\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
 class EntityManagerPackage extends BasePackage
 {
+    use AsSingleton;
+
     public function dependencies(): array
     {
         return $this->dependenciesByClass([
