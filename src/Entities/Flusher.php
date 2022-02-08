31c04dfd8761377f96bc5ae32c86f23041f3f37c
Patrick Morpey <patrick@morphp.nl>
2022-02-08 11:38:24 +0100

    Flush should not return a value

--- src/Entities/Flusher.php
+++ src/Entities/Flusher.php
@@ -6,5 +6,5 @@ namespace Medas\EntityManager\Entities;
 
 interface Flusher
 {
-    public function flush(array $entities, \SplObjectStorage $savedStates): bool;
+    public function flush(array $entities, \SplObjectStorage $savedStates): void;
 }
