3e75400b6315d18ce2d280395139cbafc07ecb24
Patrick Morpey <patrick@morphp.nl>
2022-04-04 14:54:40 +0200

    Added WhereIsNull and WhereIsNotNull

---
+++ src/Selector/Conditions/WhereIsNull.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\EntityManager\Selector\Conditions;
+
+use Medas\EntityManager\Selector\Operants\Operant;
+
+class WhereIsNull implements Condition
+{
+    public static function c(Operant $property): static
+    {
+        return new static($property);
+    }
+
+    public function __construct(
+        public Operant $property
+    )
+    {
+    }
+}
