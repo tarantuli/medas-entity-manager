36c7a727a524f931406bea35c212ea7e96ea529b
Patrick Morpey <patrick@morphp.nl>
2022-08-23 09:19:32 +0200

    Packages shouldn't have a bootstrap.php

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 use Medas\ConfigManager\ConfigManagerPackage;
+use Medas\EntityManager\EntityManagerPackage;
 use Medas\ServiceManager\Interfaces\ConfigManager;
+use Medas\ServiceManager\ServiceManager;
 
-require_once __DIR__ . '/bootstrap.php';
+$sm = ServiceManager::get();
 
-sm()->addPackage(ConfigManagerPackage::instance());
+$sm->addPackage(EntityManagerPackage::instance());
+$sm->addPackage(ConfigManagerPackage::instance());
 
 $config = service(ConfigManager::class);
 $config->addDirectory(__DIR__ . '/tests/MockUps/config');
