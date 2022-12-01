f7bb13c5a24f050a9c2c57d78e77dbed0625f4de
Patrick Morpey <patrick@morphp.nl>
2022-12-01 14:41:52 +0100

    Made State a backed enum

--- src/Entities/State.php
+++ src/Entities/State.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Entities;
 
-enum State
+enum State: int
 {
-    case Active;
-    case Inactive;
+    case Inactive = 0;
+    case Active = 1;
 }
