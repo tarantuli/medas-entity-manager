1573e80aa4a87a7530c6aabfdfcfa642a8ab8c65
Patrick Morpey <patrick@morphp.nl>
2023-02-13 15:58:25 +0100

    Fixed test file

--- tests/MockUps/MockFlusher.php
+++ tests/MockUps/MockFlusher.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace Medas\EntityManagerTest\MockUps;
 
+use Medas\EntityManager\Entities\Changes;
 use Medas\EntityManager\Entities\Flusher;
 use Medas\ServiceManager\Attributes\Service;
 
 #[Service]
 class MockFlusher implements Flusher
 {
-    public function flush(
-        array $entitiesToCreate,
-        array $entitiesToUpdate,
-        array $entitiesToDelete
-    ): void
+    public function flush(Changes $changes): void
     {
         // Do nothing
     }
