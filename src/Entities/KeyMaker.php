3fe708e343974face4c0662a2296c67a3ea7fe54
Patrick Morpey <patrick@morphp.nl>
2023-03-02 15:55:08 +0100

    Improved key making

--- src/Entities/KeyMaker.php
+++ src/Entities/KeyMaker.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Entities;
 
+use Medas\EntityManager\Attributes\HasId;
 use Medas\ServiceManager\Attributes\Service;
+use Medas\ServiceManager\Values\Interfaces\Guid;
 
 #[Service]
 class KeyMaker
 {
-    public function get(string $className, array $id): string
+    public function get(string $className, array $values): string
     {
-        foreach ($id as &$value) {
-            if (is_object($value)) {
-                $value = spl_object_id($value);
+        foreach ($values as &$value) {
+            if ($value instanceof HasId) {
+                $value = $value->id();
+            }
+
+            if ($value instanceof Guid) {
+                $value = $value->toBytes();
             }
         }
 
-        return $className . ':' . print_r($id, true);
+        return $className . ':' . serialize($values);
     }
 }
