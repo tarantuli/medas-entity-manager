7e613746bf16546c77dd508c929eabe6ebf4c273
Patrick Morpey <patrick@morphp.nl>
2023-10-02 12:40:34 +0200

    Added validation

--- src/ConfigOptions/EntityDirectories.php
+++ src/ConfigOptions/EntityDirectories.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\ConfigOptions;
 
-use Medas\Core\{Attributes\Service, Interfaces\ConfigGroup, Interfaces\ConfigOption};
+use Medas\Core\{Attributes\Service, Interfaces\ConfigGroup, Interfaces\ConfigOption, Interfaces\Validator};
 
 #[Service]
-readonly class EntityDirectories implements ConfigOption
+readonly class EntityDirectories implements ConfigOption, Validator
 {
     public function __construct(
         private EntityManagerGroup $group,
@@ -38,5 +38,10 @@ readonly class EntityDirectories implements ConfigOption
     public function default(): array
     {
         return ['src'];
+    }
+
+    public function isValid(mixed $value): bool
+    {
+        return is_array($value);
     }
 }
