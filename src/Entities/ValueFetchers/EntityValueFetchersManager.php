2531240c754feae540a3d680c6f9017bfb7d1c98
Patrick Morpey <patrick@morphp.nl>
2024-12-12 15:10:43 +0100

    Refactored EntityValueFetchersManager for better state handling
    Reorganized fetcher storage

--- src/Entities/ValueFetchers/EntityValueFetchersManager.php
+++ src/Entities/ValueFetchers/EntityValueFetchersManager.php
@@ -9,18 +9,15 @@ use Medas\Core\{Attributes\Service, Interfaces\CacheManager, Interfaces\Implemen
 #[Service]
 readonly class EntityValueFetchersManager
 {
+    private array $fetchers;
+
     public function __construct(
-        private CacheManager $cacheManager,
+        private CacheManager      $cacheManager,
+        private ImplementorFinder $implementorFinder,
     )
     {
-    }
-
-    /** @return EntityValueFetcher[] */
-    public function get(): array
-    {
         $classNames = $this->cacheManager->get()->get(__CLASS__, fn() => $this->gather());
-
-        return array_map(fn(string $name) => service($name), $classNames);
+        $this->fetchers = array_map(fn(string $name) => service($name), $classNames);
     }
 
     /** @return string[] */
@@ -28,7 +25,13 @@ readonly class EntityValueFetchersManager
     {
         return array_map(
             fn(object $service) => $service::class,
-            service(ImplementorFinder::class)->find(EntityValueFetcher::class)
+            $this->implementorFinder->find(EntityValueFetcher::class)
         );
+    }
+
+    /** @return EntityValueFetcher[] */
+    public function get(): array
+    {
+        return $this->fetchers;
     }
 }
