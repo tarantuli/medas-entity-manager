97b85f655f80fde7ff844b5e14eb3202cfce6e76
Patrick Morpey <patrick@morphp.nl>
2025-05-13 16:05:45 +0200

    Added CreateCollectionFile

--- src/ConsoleCommands/CreateCollectionFile.php
+++ src/ConsoleCommands/CreateCollectionFile.php
@@ -7,18 +7,18 @@ namespace Medas\EntityManager\ConsoleCommands;
 use Medas\Console\{Commands\ConsoleCommandGroup, Printer};
 use Medas\Core\Attributes\Service;
 use Medas\EntityManager\BaseFileCreator;
-use Medas\EntityManager\Entities\Generator\{EntityClassGenerator, FileNameFinder};
+use Medas\EntityManager\Entities\Generator\{CollectionClassGenerator, FileNameFinder};
 use Medas\FileSystem\DirectoryCreator;
 
 #[Service]
-readonly class CreateEntityFile extends BaseFileCreator
+readonly class CreateCollectionFile extends BaseFileCreator
 {
     public function __construct(
-        DirectoryCreator              $directoryCreator,
-        Printer                       $printer,
-        private EntityClassGenerator  $entityClassGenerator,
-        private EntityManagerCommands $entityManagerCommands,
-        private FileNameFinder        $fileNameFinder,
+        DirectoryCreator                 $directoryCreator,
+        Printer                          $printer,
+        private CollectionClassGenerator $collectionClassGenerator,
+        private EntityManagerCommands    $entityManagerCommands,
+        private FileNameFinder           $fileNameFinder,
     )
     {
         parent::__construct($directoryCreator, $printer);
@@ -31,20 +31,19 @@ readonly class CreateEntityFile extends BaseFileCreator
 
     public function name(): string
     {
-        return 'create-entity-file';
+        return 'create-collection-file';
     }
 
     public function description(): string
     {
-        return 'Creates an entity file for the given fully qualified class name';
+        return 'Creates an entity collection file for the given fully qualified class name';
     }
 
     public function process(array $arguments): void
     {
         $className = $arguments[1];
-        $useUuid = ($arguments[2] ?? null) !== '--id';
-        $code = $this->entityClassGenerator->generate($className, $useUuid);
-        $fileName = $this->fileNameFinder->find($className);
+        $code = $this->collectionClassGenerator->generate($className);
+        $fileName = $this->fileNameFinder->find($className . 'Collection');
 
         $this->writeToFile($code, $fileName);
     }
---
+++ src/Entities/Generator/CollectionClassGenerator.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\EntityManager\Entities\Generator;
+
+use Medas\Core\Attributes\Service;
+
+#[Service]
+readonly class CollectionClassGenerator
+{
+    private const PHP_TEMPLATE
+        = <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace {{namespace}};
+
+use Medas\EntityManager\Attributes\EntityCollection;
+use Medas\StorageManager\Entities\RecordCollection;
+
+/**
+ * @extends RecordCollection<{{shortClassName}}>
+ */
+#[EntityCollection({{shortClassName}}::class)]
+class {{shortClassName}}Collection extends RecordCollection
+{
+}
+
+PHP;
+
+    public function __construct(
+        private ClassNameNormalizer          $classNameNormalizer,
+        private FileNameFinder               $fileNameFinder,
+        private NameConverters\NameConverter $storeNameConverter,
+    )
+    {
+    }
+
+    public function generate(string $className): string
+    {
+        $className = $this->classNameNormalizer->normalize($className);
+        [$namespace, $shortClassName] = $this->splitClassName($className);
+
+        $replacements = [
+            '{{namespace}}' => $namespace,
+            '{{shortClassName}}' => $shortClassName,
+        ];
+
+        return str_replace(
+            array_keys($replacements),
+            array_values($replacements),
+            self::PHP_TEMPLATE
+        );
+    }
+
+    private function splitClassName(string $className): array
+    {
+        $pos = strrpos($className, '\\');
+
+        if ($pos === false) {
+            throw new Exceptions\ClassHasNoNamespace($className);
+        }
+
+        return [
+            substr($className, 0, $pos),
+            substr($className, $pos + 1),
+        ];
+    }
+}
