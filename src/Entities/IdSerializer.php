467bf8b0955eba21e84cb783c0eda26b5975fd3f
Patrick Morpey <patrick@morphp.nl>
2025-06-04 10:30:21 +0200

    Cast Uuid to string instead of bytes

--- src/Entities/IdSerializer.php
+++ src/Entities/IdSerializer.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Entities;
 
-use Medas\Core\{
-    Attributes\Service,
-    Interfaces\HasId,
-    Interfaces\StringSerializer,
-    Interfaces\Type,
-    Interfaces\Uuid
-};
+use Medas\Core\{Attributes\Service, Interfaces\HasId, Interfaces\StringSerializer, Interfaces\Type};
 use Medas\EntityManager\Exceptions\CannotCastValueToId;
 
 #[Service]
@@ -20,10 +14,6 @@ readonly class IdSerializer implements StringSerializer
     {
         if ($value instanceof HasId) {
             $value = $value->id();
-        }
-
-        if ($value instanceof Uuid) {
-            $value = $value->toBytes();
         }
 
         if ($value === null || is_scalar($value) || $value instanceof \Stringable) {
