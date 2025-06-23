1a7766647cac1db7c0d452167dc94efce4186f3d
Patrick Morpey <patrick@morphp.nl>
2025-06-23 14:58:49 +0200

    Refactored

--- src/MetaData/Compiler.php
+++ src/MetaData/Compiler.php
@@ -5,25 +5,21 @@ declare(strict_types=1);
 namespace Medas\EntityManager\MetaData;
 
 use Medas\Core\Attributes\Service;
-use Medas\EntityManager\{
-    Attributes,
-    Exceptions\ClassIsNotAnEntity,
-    Exceptions\EntityHasMultipleIdProperties,
-    Exceptions\EntityHasNoIdProperty,
-    Hydration\PropertyTypeNormalizer,
-    MetaData,
-    Properties\PropertyManager,
-    Types\TypeFinder
+use Medas\EntityManager\Attributes;
+use Medas\EntityManager\Exceptions\{
+    ClassIsNotAnEntity,
+    EntityHasMultipleIdProperties,
+    EntityHasNoIdProperty
 };
+use Medas\EntityManager\MetaData;
+use Medas\EntityManager\MetaData\Compiler\PropertyProcessor;
 
 #[Service]
 readonly class Compiler
 {
     public function __construct(
-        private EntityCompiler         $entityCompiler,
-        private PropertyManager        $propertyManager,
-        private PropertyTypeNormalizer $propertyTypeNormalizer,
-        private TypeFinder             $typeFinder,
+        private EntityCompiler    $entityCompiler,
+        private PropertyProcessor $propertyProcessor,
     )
     {
     }
@@ -49,113 +45,11 @@ readonly class Compiler
         $metaData->references = [];
         $metaData->inheritance = new Inheritance($class->getParentClass() ? $class->getParentClass()->name : null);
 
-        $this->processProperties($class, $metaData);
+        $this->propertyProcessor->processProperties($class, $metaData);
         $this->findIdProperty($metaData);
         $this->checkForStoreOriginalEntityType($metaData, $class);
 
         return $metaData;
-    }
-
-    private function processProperties(\ReflectionClass $class, MetaData $metaData): void
-    {
-        $properties = $this->getSortedProperties($class);
-
-        foreach ($properties as $property) {
-            $this->processProperty($property, $metaData);
-            $this->processReferences($property, $metaData);
-        }
-    }
-
-    private function getSortedProperties(\ReflectionClass $class): array
-    {
-        $parents = $this->gatherParents($class);
-        $properties = $class->getProperties();
-
-        usort(
-            $properties,
-            function (\ReflectionProperty $a, \ReflectionProperty $b) use ($parents) {
-            // Parents deeper in the chain have lower values, sorting them in front
-            return $parents[$a->class] - $parents[$b->class];
-        });
-
-        return $properties;
-    }
-
-    private function gatherParents(\ReflectionClass $parent): array
-    {
-        $counter = 0;
-        $parents = [$parent->name => $counter];
-
-        while (false !== $parent = $parent->getParentClass()) {
-            $parents[$parent->name] = --$counter;
-        }
-
-        return $parents;
-    }
-
-    private function processProperty(\ReflectionProperty $property, MetaData $metaData): void
-    {
-        if (attribute(Attributes\Unmanaged::class, $property)) {
-            return;
-        }
-
-        $type = $this->typeFinder->find($property);
-        $isNullable = $property->getType()->allowsNull();
-        $handler = $this->propertyManager->getHandler($property);
-
-        $metaData->properties[] = new Property(
-            name: $property->name,
-            type: $type,
-            hasDefault: $property->hasDefaultValue(),
-            default: $property->hasDefaultValue() ? $property->getDefaultValue() : null,
-            isId: !empty($property->getAttributes(Attributes\Id::class, \ReflectionAttribute::IS_INSTANCEOF)),
-            isGeneratedValue: !empty($property->getAttributes(Attributes\IsGeneratedValue::class)),
-            isCreationTimestamp: !empty($property->getAttributes(Attributes\IsCreationTimestamp::class)),
-            isModificationTimestamp: !empty($property->getAttributes(Attributes\IsModificationTimestmap::class)),
-            isNullable: $isNullable,
-            isUnique: !empty($property->getAttributes(Attributes\IsUnique::class)),
-            isIndex: !empty($property->getAttributes(Attributes\IsIndex::class)),
-            onDeleteCascade: !empty($property->getAttributes(Attributes\OnDeleteCascade::class)),
-            phpTypes: $this->propertyTypeNormalizer->names($property),
-            reflection: $property,
-            handler: $handler ? $handler::class : null,
-        );
-    }
-
-    private function processReferences(\ReflectionProperty $property, MetaData $metaData): void
-    {
-        $references = attribute(Attributes\References::class, $property);
-
-        if (!$references) {
-            return;
-        }
-
-        if ($references->property === null) {
-            $targetClass = new \ReflectionClass($references->entity);
-            $targetProperties = [];
-
-            foreach ($targetClass->getProperties() as $targetProperty) {
-                if (in_array($metaData->className, $this->propertyTypeNormalizer->names($targetProperty))) {
-                    $targetProperties[] = $targetProperty;
-                }
-            }
-
-            if (count($targetProperties) === 0) {
-                throw new \Exception('property ' . $property->name . ' should be referenced by ' . $references->entity . ' but no properties refer to ' . $metaData->className);
-            }
-
-            if (count($targetProperties) >= 2) {
-                throw new \Exception('property ' . $property->name . ' should be referenced by ' . $references->entity . ' but too many properties refer to ' . $metaData->className);
-            }
-
-            $references->property = $targetProperties[0]->name;
-        }
-
-        $metaData->references[] = new Reference(
-            name: $property->name,
-            entity: $references->entity,
-            property: $references->property,
-        );
     }
 
     private function findIdProperty(MetaData $metaData): void
---
+++ src/MetaData/Compiler/PropertyProcessor.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\EntityManager\MetaData\Compiler;
+
+use Medas\Core\Attributes\Service;
+use Medas\EntityManager\{
+    Attributes,
+    Hydration\PropertyTypeNormalizer,
+    MetaData,
+    MetaData\Property,
+    MetaData\Reference,
+    Properties\PropertyManager,
+    Types\TypeFinder
+};
+
+#[Service]
+readonly class PropertyProcessor
+{
+    public function __construct(
+        private PropertyManager        $propertyManager,
+        private PropertyTypeNormalizer $propertyTypeNormalizer,
+        private TypeFinder             $typeFinder,
+    )
+    {
+    }
+
+    public function processProperties(\ReflectionClass $class, MetaData $metaData): void
+    {
+        $properties = $this->getSortedProperties($class);
+
+        foreach ($properties as $property) {
+            $this->processProperty($property, $metaData);
+            $this->processReferences($property, $metaData);
+        }
+    }
+
+    private function getSortedProperties(\ReflectionClass $class): array
+    {
+        $parents = $this->gatherParents($class);
+        $properties = $class->getProperties();
+
+        usort(
+            $properties,
+            function (\ReflectionProperty $a, \ReflectionProperty $b) use ($parents) {
+            // Parents deeper in the chain have lower values, sorting them in front
+            return $parents[$a->class] - $parents[$b->class];
+        });
+
+        return $properties;
+    }
+
+    private function gatherParents(\ReflectionClass $parent): array
+    {
+        $counter = 0;
+        $parents = [$parent->name => $counter];
+
+        while (false !== $parent = $parent->getParentClass()) {
+            $parents[$parent->name] = --$counter;
+        }
+
+        return $parents;
+    }
+
+    private function processProperty(\ReflectionProperty $property, MetaData $metaData): void
+    {
+        if (attribute(Attributes\Unmanaged::class, $property)) {
+            return;
+        }
+
+        $type = $this->typeFinder->find($property);
+        $isNullable = $property->getType()->allowsNull();
+        $handler = $this->propertyManager->getHandler($property);
+
+        $metaData->properties[] = new Property(
+            name: $property->name,
+            type: $type,
+            hasDefault: $property->hasDefaultValue(),
+            default: $property->hasDefaultValue() ? $property->getDefaultValue() : null,
+            isId: !empty($property->getAttributes(Attributes\Id::class, \ReflectionAttribute::IS_INSTANCEOF)),
+            isGeneratedValue: !empty($property->getAttributes(Attributes\IsGeneratedValue::class)),
+            isCreationTimestamp: !empty($property->getAttributes(Attributes\IsCreationTimestamp::class)),
+            isModificationTimestamp: !empty($property->getAttributes(Attributes\IsModificationTimestmap::class)),
+            isNullable: $isNullable,
+            isUnique: !empty($property->getAttributes(Attributes\IsUnique::class)),
+            isIndex: !empty($property->getAttributes(Attributes\IsIndex::class)),
+            onDeleteCascade: !empty($property->getAttributes(Attributes\OnDeleteCascade::class)),
+            phpTypes: $this->propertyTypeNormalizer->names($property),
+            reflection: $property,
+            handler: $handler ? $handler::class : null,
+        );
+    }
+
+    private function processReferences(\ReflectionProperty $property, MetaData $metaData): void
+    {
+        $references = attribute(Attributes\References::class, $property);
+
+        if (!$references) {
+            return;
+        }
+
+        if ($references->property === null) {
+            $targetClass = new \ReflectionClass($references->entity);
+            $targetProperties = [];
+
+            foreach ($targetClass->getProperties() as $targetProperty) {
+                if (in_array($metaData->className, $this->propertyTypeNormalizer->names($targetProperty))) {
+                    $targetProperties[] = $targetProperty;
+                }
+            }
+
+            if (count($targetProperties) === 0) {
+                throw new \Exception('property ' . $property->name . ' should be referenced by ' . $references->entity . ' but no properties refer to ' . $metaData->className);
+            }
+
+            if (count($targetProperties) >= 2) {
+                throw new \Exception('property ' . $property->name . ' should be referenced by ' . $references->entity . ' but too many properties refer to ' . $metaData->className);
+            }
+
+            $references->property = $targetProperties[0]->name;
+        }
+
+        $metaData->references[] = new Reference(
+            name: $property->name,
+            entity: $references->entity,
+            property: $references->property,
+        );
+    }
+}
