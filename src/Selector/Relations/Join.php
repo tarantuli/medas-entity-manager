f271bef7759bc10c9b14407960193c2e5d818bd7
Patrick Morpey <patrick@morphp.nl>
2025-06-24 12:14:25 +0200

    Added Join::c()

--- src/Selector/Relations/Join.php
+++ src/Selector/Relations/Join.php
@@ -6,6 +6,15 @@ namespace Medas\EntityManager\Selector\Relations;
 
 class Join implements Relation
 {
+    public static function c(
+        string      $targetEntity,
+        string|null $sourceProperty = null,
+        string|null $targetProperty = null
+    ): static
+    {
+        return new static($targetEntity, $sourceProperty, $targetProperty);
+    }
+
     public function __construct(
         public string      $targetEntity,
         public string|null $sourceProperty = null,
