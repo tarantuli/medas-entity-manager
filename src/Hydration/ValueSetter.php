163909c134b392ec9ffc4aab5e815333b1f5fc29
Patrick Morpey <patrick@morphp.nl>
2025-07-02 10:45:56 +0200

    Extracted ValueCaster

--- src/Hydration/ValueSetter.php
+++ src/Hydration/ValueSetter.php
@@ -4,25 +4,14 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Hydration;
 
-use Medas\Core\{
-    Attributes\Service,
-    Interfaces\Collection,
-    Interfaces\TracksChanges,
-    Interfaces\Uuid,
-    Interfaces\UuidProvider
-};
-use Medas\EntityManager\{
-    Attributes\EntityCollection,
-    Entities\Initializer,
-    Exceptions\InvalidPropertyType,
-    MetaData
-};
+use Medas\Core\Attributes\Service;
+use Medas\EntityManager\MetaData;
 
 #[Service]
 readonly class ValueSetter
 {
     public function __construct(
-        private UuidProvider|null $uuidProvider,
+        private ValueCaster $valueCaster,
     )
     {
     }
@@ -52,97 +41,7 @@ readonly class ValueSetter
             return;
         }
 
-        $valueType = get_debug_type($value);
-
-        if (!$property->allowsPhpType($valueType) && $value !== null) {
-            foreach ($property->phpTypes as $phpType) {
-                if ($phpType === \DateTime::class) {
-                    $value = new \DateTime($value);
-                    $valueType = $phpType;
-
-                    break;
-                }
-
-                if ($phpType === Uuid::class && is_string($value)) {
-                    $value = $this->uuidProvider->fromString($value);
-                }
-
-                if (enum_exists($phpType)) {
-                    $backingType = (new \ReflectionEnum($phpType))->getBackingType();
-
-                    if (((string) $backingType === 'int') && is_string($value)) {
-                        $value = intval($value);
-                    }
-
-                    $value = $phpType::from($value);
-                    $valueType = $phpType;
-
-                    break;
-                }
-
-                if (class_exists($phpType)) {
-                    if ($attribute = attribute(EntityCollection::class, new \ReflectionClass($phpType))) {
-                        // We cannot inject the Initializer in the constructor because that already depends on this class
-                        $initializer = service(Initializer::class);
-                        $collection = new $phpType();
-                        $itemType = $attribute->contentType;
-
-                        foreach ($value as $itemValues) {
-                            $collection[] = $initializer->initialize($itemType, $itemValues);
-                        }
-
-                        if ($collection instanceof TracksChanges) {
-                            $collection->resetChangeTracking();
-                        }
-
-                        $value = $collection;
-                    }
-                    elseif (!$value instanceof Collection) {
-                        $value = em()->get($phpType, $value);
-                    }
-
-                    $valueType = $phpType;
-
-                    break;
-                }
-
-                if (interface_exists($phpType) && $value instanceof $phpType) {
-                    $valueType = $phpType;
-
-                    break;
-                }
-
-                if ($phpType === 'int' && preg_match('/^\d+$/', $value)) {
-                    $value = (int) $value;
-                    $valueType = 'int';
-
-                    break;
-                }
-
-                if ($phpType === 'bool' && is_int($value)) {
-                    $value = (bool) $value;
-                    $valueType = 'bool';
-
-                    break;
-                }
-
-                if ($phpType === 'float' && is_int($value)) {
-                    $value = (float) $value;
-                    $valueType = 'float';
-
-                    break;
-                }
-            }
-        }
-
-        if (!$property->allowsPhpType($valueType)) {
-            throw new InvalidPropertyType(
-                $metaData->className,
-                $propertyName,
-                $valueType,
-                $property->phpTypes
-            );
-        }
+        $value = $this->valueCaster->cast($metaData, $property, $value);
 
         $property->reflection->setValue($entity, $value);
     }
