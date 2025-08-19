f89577dcfedb6995fd34e3440d8618cd50d7b131
Patrick Morpey <patrick@morphp.nl>
2025-08-19 11:31:36 +0200

    WIP on selectors

--- src/Selector/Grouping/GroupBy.php
+++ src/Selector/Grouping/GroupBy.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Selector\Grouping;
 
-use Medas\EntityManager\Selector\{Element, Operants\Operant};
+use Medas\EntityManager\Selector\{Element, Operants\Property};
 
 class GroupBy implements Element
 {
-    public static function c(Operant $operant): static
+    public static function c(Property $property): static
     {
-        return new static($operant);
+        return new static($property);
     }
 
     public function __construct(
-        public Operant $operant,
+        public Property $property,
     )
     {
     }
