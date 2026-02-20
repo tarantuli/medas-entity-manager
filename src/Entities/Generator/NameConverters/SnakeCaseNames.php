864c784736e2aacac61f6eb05b6842269782c602
Patrick Morpey <patrick@morphp.nl>
2026-02-20 16:53:47 +0100

    Updated deps

--- src/Entities/Generator/NameConverters/SnakeCaseNames.php
+++ src/Entities/Generator/NameConverters/SnakeCaseNames.php
@@ -4,21 +4,20 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Entities\Generator\NameConverters;
 
-use Medas\Core\{Attributes\Service, IdentifierMaker};
+use Medas\Core\{Attributes\Service, Identifier};
 use Medas\EntityManager\Entities\Generator\Pluralizers\Pluralizer;
 
 #[Service]
 readonly class SnakeCaseNames implements NameConverter
 {
     public function __construct(
-        private IdentifierMaker $identifierMaker,
-        private Pluralizer      $pluralizer,
+        private Pluralizer $pluralizer,
     )
     {
     }
 
     public function convert(string $name): string
     {
-        return $this->identifierMaker->fromCamelCase($this->pluralizer->pluralize($name))->toSnakeCase();
+        return new Identifier($this->pluralizer->pluralize($name))->toSnakeCase();
     }
 }
