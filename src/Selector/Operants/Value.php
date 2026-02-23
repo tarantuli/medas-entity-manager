c9ede669414c91f7d3e0286a8ffdd79a810c43e2
Patrick Morpey <patrick@morphp.nl>
2026-02-23 16:38:18 +0100

    Added option to mark Value operants explicitly as trusted

--- src/Selector/Operants/Value.php
+++ src/Selector/Operants/Value.php
@@ -6,13 +6,14 @@ namespace Medas\EntityManager\Selector\Operants;
 
 class Value implements Operant
 {
-    public static function c(mixed $value): static
+    public static function c(mixed $value, bool $isTrusted = false): static
     {
-        return new static($value);
+        return new static($value, $isTrusted);
     }
 
     public function __construct(
         public mixed $value,
+        public bool  $isTrusted = false,
     )
     {
     }
