ada093e69ab2df23edef3cf22af81c6260372e9f
Patrick Morpey <patrick@morphp.nl>
2026-06-15 12:17:45 +0200

    Updated dependencies to use `medas-core` v4 and aligned related packages; adjusted `ServiceConfig` to `ServiceConfigBuilder` in `phpunit.bootstrap.php`.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -9,24 +9,23 @@ use Medas\EntityManager\EntityManagerPackage;
 use Medas\EntityManagerTest\MockUps\MockUpPackage;
 use Medas\Events\EventsPackage;
 use Medas\ObjectInstantiator\ObjectInstantiator;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(
-    function (): ServiceConfig {
-        $config = new ServiceConfig(ObjectInstantiator::class);
-        $config->addPackages([
-            ConfigManagerPackage::instance(),
-            ConfigOptionsPackage::instance(),
-            EntityManagerPackage::instance(),
-            EventsPackage::instance(),
-            MockUpPackage::instance(),
-        ]);
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
-        return $config;
-    }
-);
+    $config->addPackages([
+        ConfigManagerPackage::instance(),
+        ConfigOptionsPackage::instance(),
+        EntityManagerPackage::instance(),
+        EventsPackage::instance(),
+        MockUpPackage::instance(),
+    ]);
+
+    return $config;
+});
 
 service(ConfigManager::class)
     ->addDirectory(__DIR__ . '/tests/MockUps/config');
