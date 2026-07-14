f953db134ed28326da77e5acb3cd061385378c22
Patrick Morpey <patrick@morphp.nl>
2026-07-14 15:19:33 +0200

    Added support for `Period` type in `ValueCaster`.

---
+++ src/Selector/Conditions/WhereIsLessThanOrEqual.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\EntityManager\Selector\Conditions;
+
+class WhereIsLessThanOrEqual extends WhereIs
+{
+}
---
+++ src/Selector/Conditions/WhereIsMoreThanOrEqual.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\EntityManager\Selector\Conditions;
+
+class WhereIsMoreThanOrEqual extends WhereIs
+{
+}
