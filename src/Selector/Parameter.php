b2a2bfdf925d023355158e16bc5b367b2db16aca
Patrick Morpey <patrick@morphp.nl>
2026-07-30 16:48:21 +0200

    Added optional `Type` parameter to `Parameter` constructor and `c()` method

--- src/Selector/Parameter.php
+++ src/Selector/Parameter.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 namespace Medas\EntityManager\Selector;
 
+use Medas\Core\Interfaces\Type;
+
 class Parameter implements Element
 {
-    public static function c(string $name, bool $hasDefault = false, mixed $default = null): static
+    public static function c(
+        string    $name,
+        bool      $hasDefault = false,
+        mixed     $default = null,
+        Type|null $type = null
+    ): static
     {
-        return new static($name, $hasDefault, $default);
+        return new static($name, $hasDefault, $default, $type);
     }
 
     public function __construct(
-        public string $name,
-        public bool   $hasDefault = false,
-        public mixed  $default = null,
+        public string    $name,
+        public bool      $hasDefault = false,
+        public mixed     $default = null,
+        public Type|null $type = null,
     )
     {
     }
